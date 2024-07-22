37df7b6d06ecc7c59e59601f4074dc3afea81776
liamkrieling <liam@stubber.com>
2024-07-22 11:24:55 +0000

    fixing console.logs

--- src/lib/stores/config_store.js
+++ src/lib/stores/config_store.js
@@ -4,10 +4,6 @@ import { writable } from "svelte/store";
 export const fullscreen = writable(false);
 export const fullscreen_toggle = writable(false);
 
-fullscreen_toggle.subscribe(why => {
-  console.log('fullscreen_toggle',why)
-})
-
 export const switching_opened = writable(false);
 export const switch_whatsapp = writable(false);
 export const switch_sms = writable(false);
