24f25646851dd54930d094886dbe70078c3a43c1
liamkrieling <liam@stubber.com>
2024-11-19 18:08:10 +0200

    fixing voice note selection

--- src/lib/shared/service_upload.js
+++ src/lib/shared/service_upload.js
@@ -211,7 +211,7 @@ export const payload_buffer_worker = async (payload) => {
     let form = new FormData();
     let convert_voice_note = false;
 
-    if (payload.attachments[0].blob.type.includes("mp4")) {
+    if (payload.attachments[0].blob.type.includes("mp4") && payload.message.type == "voice") {
       convert_voice_note = true;
     }
 
